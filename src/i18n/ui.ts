41e5305b66ddfab43405771c8cd7a9243a95b03d
clys-man <clysmanalves@gmail.com>
2025-09-07 10:22:12 -0300

    feat: add header translations

---
+++ src/i18n/ui.ts
@@ -0,0 +1,33 @@
+export const languages = {
+  en: 'English',
+  pt_BR: 'Português',
+};
+
+export const defaultLang = 'pt_BR';
+
+export const ui = {
+  en: {
+    'nav.skip-content': 'Skip Content',
+    'nav.about': 'About',
+    'nav.posts': 'Posts',
+    'nav.tags': 'Tags',
+    'nav.archives': 'Archives',
+    'nav.search': 'Search',
+    'nav.theme': 'Change theme',
+
+    'main.description': 'lorem ipsum dolor sit amet, consectetur adipiscing elit. mauris in ultrices augue, eget viverra dolor. cras sit amet tellus euismod, aliquet libero non, tempus mi. etiam fringilla odio et lacus efficitur tempor. nullam ultricies aliquam quam a porttitor. suspendisse id blandit tellus, eu sodales enim. curabitur molestie malesuada luctus. duis in nunc ut tellus varius vulputate. proin finibus ligula at turpis suscipit fringilla. ',
+    'main.social-links': 'Social Links',
+  },
+  pt_BR: {
+    'nav.skip-content': 'Pular para o conteúdo',
+    'nav.about': 'Sobre',
+    'nav.posts': 'Postagens',
+    'nav.tags': 'Tags',
+    'nav.archives': 'Arquivos',
+    'nav.search': 'Buscar',
+    'nav.theme': 'Mudar de tema',
+
+    'main.description': 'lorem ipsum dolor sit amet, consectetur adipiscing elit. mauris in ultrices augue, eget viverra dolor. cras sit amet tellus euismod, aliquet libero non, tempus mi. etiam fringilla odio et lacus efficitur tempor. nullam ultricies aliquam quam a porttitor. suspendisse id blandit tellus, eu sodales enim. curabitur molestie malesuada luctus. duis in nunc ut tellus varius vulputate. proin finibus ligula at turpis suscipit fringilla. ',
+    'main.social-links': 'Redes Sociais',
+  },
+} as const;
